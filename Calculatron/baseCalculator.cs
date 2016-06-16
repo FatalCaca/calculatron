c98be86cf909f9c3d1185e6d917242695f6c88ac
arnonaud <arnaud.lemarquand@gmail.com>
2016-06-16 14:45:05 +0200

    ajout method de base

--- Calculatron/baseCalculator.cs
+++ Calculatron/baseCalculator.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Calculatron
 {
-    class baseCalculator
+    public class baseCalculator
     {
+        public float Addition(float valeurA, float valeurB)
+        {
+            return valeurA + valeurB;
+        }
+
+        public float Soustraction(float valeurA, float valeurB)
+        {
+            return valeurA - valeurB;
+        }
+
+        public float Division(float valeurA, float valeurB)
+        {
+            float value;
+            if(valeurB != 0)
+            {
+                value = valeurA / valeurB;
+            }
+            else
+            {
+                value = -1;
+            }
+            
+            return value;
+        }
+
+        public float Multiplication(float valeurA, float valeurB)
+        {
+            return valeurA * valeurB;
+        }
     }
 }
