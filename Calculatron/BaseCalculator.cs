e4f55e8ab56bd053c971403d2924135c712e3aab
FatalCaca <simon.ballu@gmail.com>
2016-06-16 14:59:24 +0200

    added some documentation

--- Calculatron/BaseCalculator.cs
+++ Calculatron/BaseCalculator.cs
@@ -8,16 +8,19 @@ namespace Calculatron
 {
     public class BaseCalculator
     {
+        /// Return the result of an addition between 2 numbers
         public float Addition(float valeurA, float valeurB)
         {
             return valeurA + valeurB;
         }
 
+        /// Return the result of a substraction between 2 numbers
         public float Soustraction(float valeurA, float valeurB)
         {
             return valeurA - valeurB;
         }
 
+        /// Returns the result of a division between 2 numbers
         public float Division(float valeurA, float valeurB)
         {
             float result;
@@ -34,6 +37,7 @@ namespace Calculatron
             return result;
         }
 
+        /// Returns the product of 2 numbers
         public float Multiplication(float valeurA, float valeurB)
         {
             return valeurA * valeurB;
